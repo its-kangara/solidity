486de25931e78eba07dd76c65915af9c020169c6
its-kangara <davidkangara83@gmail.com>
2023-01-17 00:19:44 +0300

    day2

--- contracts/Retrieve-gas.sol
+++ contracts/Retrieve-gas.sol
@@ -4,7 +4,24 @@ pragma solidity ^0.8.8;
 contract SimpleStorage {
     uint256 public  favouriteNumber;  //if you dont set the number, the number is automatically initialized to 0
                                     //if you dont specify the visiblity of a funtion is by default set to Internal 
+    //uint256 public favouriteNumber;
     
+    //not the ideal way
+    ///People public person = People({favouriteNumber: 2, name: "Kangara"});
+  
+  
+   struct People {
+       uint256 favouriteNumber;
+       string name;
+       uint256 age;
+   }
+   //array 
+    People[] public  people;
+
+    function addperson (string memory _name, uint256 _favouriteNumber, uint256 _age) public {
+        People memory newperson = People({favouriteNumber:_favouriteNumber, name: _name, age: _age});
+        people.push(newperson);
+    }
    function store(uint256 _favouriteNumber) public{
        favouriteNumber = _favouriteNumber;
        retrieve();
